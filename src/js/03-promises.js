88ecfce9008e87ed42150b02e0fc452da802cee7
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-02-07 22:59:40 +0100

    hw-9_3_start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,29 @@
+//! Завдання 3 - генератор промісів
+// Виконуй це завдання у файлах 03-promises.html і 03-promises.js. Подивися демо-відео роботи генератора промісів.
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах, крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+
+// <form class="form">
+//   <label>
+//     First delay (ms)
+//     <input type="number" name="delay" required />
+//   </label>
+//   <label>
+//     Delay step (ms)
+//     <input type="number" name="step" required />
+//   </label>
+//   <label>
+//     Amount
+//     <input type="number" name="amount" required />
+//   </label>
+//   <button type="submit">Create promises</button>
+// </form>
+
+//todo Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay)
+//todo стільки разів, скільки ввели в поле amount.
+//todo Під час кожного виклику передай їй номер промісу (position), що створюється,
+//todo і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
 // function createPromise(position, delay) {
 //   const shouldResolve = Math.random() > 0.3;
 //   if (shouldResolve) {
@@ -6,6 +32,101 @@
 //     // Reject
 //   }
 // }
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+// Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
+//?==========================================================
+//* імпортуємо бібліотеку notiflix
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+//* доступ до інпутів
+const refs = {
+  form: document.querySelector('.form'),
+
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+
+  submit: document.querySelector('button[type="submit"]'),
+};
+
+refs.form.addEventListener('input', onDateInput);
+
+function onDateInput() {
+  console.log('zis input =>');
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+
+  //   .then(({ position, delay }) => {
+  //     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  //   })
+  //   .catch(({ position, delay }) => {
+  //     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  //   });
+}
+//
+// createPromise(2, 1500)
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+///
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 //?========================================
 // const form = document.querySelector('.form');
