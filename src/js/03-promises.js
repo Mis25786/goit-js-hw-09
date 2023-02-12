0ae82be29f8a2baef427d7fdaceef947afe445e8
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-02-12 18:58:06 +0100

    hw-9_promis_correction

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -95,7 +95,7 @@ function onSubmit(e) {
   let amount = +e.currentTarget.elements.amount.value;
 
   //* викликати функцію - вказану кількість разів і виводимо повідомлення
-  for (let i = 0; i <= amount; i += 1) {
+  for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
         console.log(
